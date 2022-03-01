5b68fd0bae6f78a1f6b0901c10b0b1bef9f50d36
Henrik Shadman <henrik@Henriks-MacBook-Pro.local>
2022-03-01 16:02:59 +0100

    försök 2

---
+++ src/index.js
@@ -0,0 +1,11 @@
+//YOUYOYO PUTIN IS A DICK
+//Ukraine foreva ahley
+//YOYOYOYO / Ludde
+//hej hej /henrik
+//snälla funka /henrik
+
+
+console.log("joel är en sjuk man")
+console.log("Github är coooooooolt, nästan lika coolt som SångisLudde")
+
+console.log("Nu är Ashley back");
